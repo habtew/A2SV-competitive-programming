154497dc20c9fd9f08b0925f6830679a988490da
habtew <88963704+habtew@users.noreply.github.com>
2024-02-13 18:20:17 +0530

    Time: 44 ms (14.18%), Space: 16.5 MB (66.64%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        result = []
+        for word in words:
+            word_set = set(word.lower())
+            if word_set <= set("qwertyuiop") or word_set <= set("asdfghjkl") or word_set <= set("zxcvbnm"):
+                result.append(word) 
+
+        return result
+            
+
