2cb1ffc14b42052dce481f345c8d24035c7458e4
habtew <88963704+habtew@users.noreply.github.com>
2024-02-16 18:57:09 +0530

    Time: 488 ms (54.22%), Space: 71.9 MB (97.78%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -0,0 +1,28 @@
+class FrequencyTracker:
+    def __init__(self) -> None:
+        self.number_frequency = Counter()  
+        self.frequency_count = Counter()
+        
+    def add(self, number):
+        self.number_frequency[number] += 1
+        self.frequency_count[self.number_frequency[number]] += 1
+        self.frequency_count[self.number_frequency[number] - 1] -= 1
+        
+    def deleteOne(self, number):
+        if self.number_frequency[number] > 0:
+            self.number_frequency[number] -= 1
+            if self.number_frequency[number] == 0:
+                del self.number_frequency[number]
+            self.frequency_count[self.number_frequency[number]] += 1
+            self.frequency_count[self.number_frequency[number] + 1] -= 1
+    
+    def hasFrequency(self, frequency):
+        return self.frequency_count[frequency] > 0
+
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
