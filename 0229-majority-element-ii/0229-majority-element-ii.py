6b7afaeb454c54b9e475e19a9a3f12846f9e66e7
habtew <88963704+habtew@users.noreply.github.com>
2024-02-27 01:22:09 +0530

    Time: 101 ms (50.44%), Space: 18 MB (50.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        # [2,2,,64,4,6] == 6 elements
-        # 6/ 3 = 2 , element more than twice [2, 4, 6]
         nums_count = Counter(nums)
         n = len(nums)
         occur = n // 3
