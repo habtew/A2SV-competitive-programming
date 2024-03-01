6d05750d2850f114dfa12c7f5a087440315011ce
habtew <88963704+habtew@users.noreply.github.com>
2024-03-01 12:40:09 +0530

    Time: 39 ms (64.52%), Space: 16.8 MB (40.10%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,8 +1,9 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         friends_list = list(range(1, n + 1))
-
+#         the index of current friend
         current_index = 0
+        
         while len(friends_list) > 1:
             current_index = (current_index + k - 1) % len(friends_list)
             friends_list.pop(current_index)
