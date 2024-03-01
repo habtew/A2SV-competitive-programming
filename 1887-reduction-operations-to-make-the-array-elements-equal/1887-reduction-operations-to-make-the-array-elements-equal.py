8f11e5197f7fa44600b4375becd9cefd79f7dfe4
habtew <88963704+habtew@users.noreply.github.com>
2024-03-01 23:31:49 +0530

    Time: 768 ms (46.78%), Space: 24.1 MB (72.73%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,16 +1,11 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        element_indices = defaultdict(list)
-        unique_sorted_elements = sorted(set(nums))
-
-        if len(unique_sorted_elements) == 1:
-            return 0
-
-        for index, element in enumerate(nums):
-            element_indices[element].append(index)
-
-        total_operations = 0
-        for index, unique_element in enumerate(unique_sorted_elements):
-            total_operations += index * len(element_indices[unique_element])
-
-        return total_operations
+        nums.sort()
+        n = len(nums)
+        prev = nums[0]
+        count = 0
+        for i in range(1, n):
+            if prev != nums[i]:
+                count += n - i
+                prev = nums[i]
+        return count
